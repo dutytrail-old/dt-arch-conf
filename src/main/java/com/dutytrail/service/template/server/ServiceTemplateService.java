d0870f4928ec14c7cb4e228240bdf9f8f9ff9a4b
joan.tolos <joan.tolos@thomsonreuters.com>
2016-04-21 12:58:37 +0200

    Un secure connection with Eureka and config service working

--- src/main/java/com/dutytrail/service/template/server/ServiceTemplateService.java
+++ src/main/java/com/dutytrail/service/template/server/ServiceTemplateService.java
@@ -1,5 +1,6 @@
 package com.dutytrail.service.template.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServiceTemplateService {
 
+    @Value("${ping.alive}")
+    private String configPingAlive;
+
     @RequestMapping(value = "/ping", method = RequestMethod.GET, produces = "application/json")
     public String ping() {
-        return "Service Template -> Alive";
+        return "Service Template -> Alive \nConfig Profile: "+this.configPingAlive;
     }
 
 }
