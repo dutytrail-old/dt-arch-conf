16794c549b06f0d537f062b6d256e101a7789eb0
joan.tolos <joan.tolos@thomsonreuters.com>
2016-04-21 16:51:09 +0200

    Bug fixed

--- src/main/java/com/dutytrail/arch/conf/starter/ArchConf.java
+++ src/main/java/com/dutytrail/arch/conf/starter/ArchConf.java
@@ -2,6 +2,7 @@ package com.dutytrail.arch.conf.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,10 +11,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @EnableEurekaClient
+@EnableConfigServer
 @ComponentScan(basePackages = {"com.dutytrail.arch.conf"})
 public class ArchConf {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ArchConf.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ArchConf.class, args);
+    }
+
 }
